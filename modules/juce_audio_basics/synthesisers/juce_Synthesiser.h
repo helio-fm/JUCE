f8a60ce5d32acfcda1f3b02ebcd30c3589b0ed79
Peter Rudenko <peter.rudenko@gmail.com>
2024-06-22 09:24:41 +0400

    Synthesiser: expose the voice's current channel number

--- modules/juce_audio_basics/synthesisers/juce_Synthesiser.h
+++ modules/juce_audio_basics/synthesisers/juce_Synthesiser.h
@@ -209,6 +209,8 @@ public:
     */
     virtual bool isPlayingChannel (int midiChannel) const;
 
+    virtual int getCurrentPlayingChannel() const noexcept       { return currentPlayingMidiChannel; }
+
     /** Returns the current target sample rate at which rendering is being done.
         Subclasses may need to know this so that they can pitch things correctly.
     */
